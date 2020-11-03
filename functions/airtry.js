59a4decac1a49c054685a166e6eaa89074f3535a
Gabriel <gabriel.cesar.pereira.9@gmail.com>
2020-11-03 18:13:33 -0300

    my second commit

---
+++ functions/airtry.js
@@ -0,0 +1,28 @@
+exports.handler = function(event, context, callback) {
+    var Airtable = require ('airtable');
+    var base = new Airtable({apiKey:'keyFqKmXCtU2IkZGw'}).base(
+    'app1ANJMB2FcVdb5o');
+    /*base('Table 1').select({
+        maxRecords: 3,
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage){
+        records.forEach(function(record){
+            console.log('Retrieved', record.get('Name'));
+        });
+        fetchNextPage();}, function done(err){
+            if (err) {console.error(err); return;}
+        });*/
+    base('Table 1').create([{
+        "fields":{
+            "Name":"5",
+            "login":"Gabriel",
+            "senha":"gabriel123",
+            "atributo1":"ok"
+        }
+    }], function(err, records){
+        if (err){ console.error(err);
+        return;}records.forEach(function(record){
+            console.log(record.getId());
+        });
+    })
+    }
