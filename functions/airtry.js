6ac2bc98c88804c242bf59036563264a38b8b519
Gabriel <gabriel.cesar.pereira.9@gmail.com>
2020-11-03 18:30:18 -0300

    fixing ; airtry

--- functions/airtry.js
+++ functions/airtry.js
@@ -2,7 +2,7 @@ exports.handler = function(event, context, callback) {
     var Airtable = require ('airtable');
     var base = new Airtable({apiKey:'keyFqKmXCtU2IkZGw'}).base(
     'app1ANJMB2FcVdb5o');
-    console.log('this is working')
+    console.log('this is working');
     /*base('Table 1').select({
         maxRecords: 3,
         view: "Grid view"
